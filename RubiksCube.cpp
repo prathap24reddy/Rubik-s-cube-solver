a0d6f96a8f9a2e694325b2a51a1160379dd02746
prathap24reddy <prathap24reddy@gmail.com>
2024-08-03 12:13:10 +0530

    added RubiksCube

---
+++ RubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by prath on 03-08-2024.
+//
+
+#include "RubiksCube.h"
---
+++ RubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by prath on 03-08-2024.
+//
+
+#ifndef RUBIKSCUBE_H
+#define RUBIKSCUBE_H
+
+
+
+class RubiksCube {
+
+};
+
+
+
+#endif //RUBIKSCUBE_H
